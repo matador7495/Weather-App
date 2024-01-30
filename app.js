2d936771bd42a7398f18cdbf672131f3f85f0390
mr.nobody <id4acc2014@protonmail.com>
2024-01-30 14:43:31 +0330

    initHandler

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import getWeatherData from "./utils/httpReq.js";
 import { removeModal, showModal } from "./utils/modal.js";
+import { getWeekDay } from "./utils/customeDate.js";
 
-const DAYS = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 const searchInput = document.querySelector("input");
 const searchButton = document.querySelector("button");
@@ -28,9 +28,7 @@ const renderCurrentWeather = (data) => {
   weatherContainer.innerHTML = weatherJSX;
 };
 
-const getWeekDay = (date) => {
-  return DAYS[new Date(date * 1000).getDay()];
-};
+
 
 const renderForecastWeather = (data) => {
   if (!data) return;
@@ -87,6 +85,15 @@ const locationHandler = () => {
   }
 };
 
+const initHandler = async () => {
+  const currentData = await getWeatherData("current", "qorveh");
+  renderCurrentWeather(currentData);
+
+  const forecastData = await getWeatherData("forecast", "qorveh");
+  renderForecastWeather(forecastData);
+};
+
 searchButton.addEventListener("click", searchHandler);
 locationIcon.addEventListener("click", locationHandler);
 modalButton.addEventListener("click", removeModal);
+document.addEventListener("DOMContentLoaded", initHandler);
