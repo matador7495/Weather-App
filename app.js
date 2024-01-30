4184ca8b0191bf9a310e2b6054f27603fda3088c
mr.nobody <id4acc2014@protonmail.com>
2024-01-30 18:10:48 +0330

    apply #loader & Responsive

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ import getWeatherData from "./utils/httpReq.js";
 import { removeModal, showModal } from "./utils/modal.js";
 import { getWeekDay } from "./utils/customeDate.js";
 
-
 const searchInput = document.querySelector("input");
 const searchButton = document.querySelector("button");
 const weatherContainer = document.getElementById("weather");
@@ -16,8 +15,8 @@ const renderCurrentWeather = (data) => {
     <h1>${data.name}, ${data.sys.country}</h1>
     <div id="main">
       <img alt="weather icon" src="https://openweathermap.org/img/w/${data.weather[0].icon}.png" />
-      <span>${data.weather[0].main}</span>
-      <p>${Math.round(data.main.temp)} °C</p>
+      <span>${data.weather[0].description}</span>
+      <p>${Math.round(data.main.temp)}°C</p>
     </div>
     <div id="info">
       <p>Humidity: <span>${data.main.humidity} %</span></p>
@@ -27,8 +26,6 @@ const renderCurrentWeather = (data) => {
 
   weatherContainer.innerHTML = weatherJSX;
 };
-
-
 
 const renderForecastWeather = (data) => {
   if (!data) return;
@@ -41,7 +38,7 @@ const renderForecastWeather = (data) => {
       <div>
         <img alt="weather icon" src="https://openweathermap.org/img/w/${i.weather[0].icon}.png" />
         <h3>${getWeekDay(i.dt)}</h3>
-        <p>${Math.round(i.main.temp)} °C</p>
+        <p>${Math.round(i.main.temp)}°C</p>
         <span>${i.weather[0].main}</span>
       </div>
     `;
@@ -63,6 +60,8 @@ const searchHandler = async () => {
 
   const forecastData = await getWeatherData("forecast", cityName);
   renderForecastWeather(forecastData);
+
+  searchInput.value = "";
 };
 
 const positionCallback = async (position) => {
