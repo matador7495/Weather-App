9f68ae741f3e40146bb49f95cb06eb130252a864
mr.nobody <id4acc2014@protonmail.com>
2024-01-30 14:03:44 +0330

    build modal
    change console.log to showModal in app-httpReq.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import getWeatherData from "./utils/httpReq.js";
+import { removeModal, showModal } from "./utils/modal.js";
 
 const DAYS = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
@@ -7,10 +8,10 @@ const searchButton = document.querySelector("button");
 const weatherContainer = document.getElementById("weather");
 const forecastContainer = document.getElementById("forecast");
 const locationIcon = document.getElementById("location-icon");
-
-
+const modalButton = document.getElementById("modal-button");
 
 const renderCurrentWeather = (data) => {
+  if (!data) return;
   const weatherJSX = `
     <h1>${data.name}, ${data.sys.country}</h1>
     <div id="main">
@@ -32,16 +33,18 @@ const getWeekDay = (date) => {
 };
 
 const renderForecastWeather = (data) => {
+  if (!data) return;
+
   forecastContainer.innerHTML = "";
   data = data.list.filter((obj) => obj.dt_txt.endsWith("12:00:00"));
 
   data.forEach((i) => {
     const forecastJSX = `
       <div>
-      <img alt="weather icon" src="https://openweathermap.org/img/w/${i.weather[0].icon}.png" />
-      <h3>${getWeekDay(i.dt)}</h3>
-      <p>${Math.round(i.main.temp)} °C</p>
-      <span>${i.weather[0].main}</span>
+        <img alt="weather icon" src="https://openweathermap.org/img/w/${i.weather[0].icon}.png" />
+        <h3>${getWeekDay(i.dt)}</h3>
+        <p>${Math.round(i.main.temp)} °C</p>
+        <span>${i.weather[0].main}</span>
       </div>
     `;
 
@@ -53,35 +56,37 @@ const searchHandler = async () => {
   const cityName = searchInput.value;
 
   if (!cityName) {
-    alert("Please enter city name");
+    showModal("Please enter city name");
+    return;
   }
 
-  const currentData = await getWeatherData("current",cityName);
+  const currentData = await getWeatherData("current", cityName);
   renderCurrentWeather(currentData);
 
-  const forecastData = await getWeatherData("forecast",cityName);
+  const forecastData = await getWeatherData("forecast", cityName);
   renderForecastWeather(forecastData);
 };
 
 const positionCallback = async (position) => {
-  const currentData = await getWeatherData("current",position.coords);
+  const currentData = await getWeatherData("current", position.coords);
   renderCurrentWeather(currentData);
 
-  const forecastData = await getWeatherData("forecast",position.coords);
+  const forecastData = await getWeatherData("forecast", position.coords);
   renderForecastWeather(forecastData);
 };
 
 const errorCallback = (error) => {
-  console.log(error.message);
+  showModal(error.message);
 };
 
 const locationHandler = () => {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(positionCallback, errorCallback);
   } else {
-    alert("Geolocation is not supported by this browser.");
+    showModal("Geolocation is not supported by this browser.");
   }
 };
 
 searchButton.addEventListener("click", searchHandler);
 locationIcon.addEventListener("click", locationHandler);
+modalButton.addEventListener("click", removeModal);
--- utils/httpReq.js
+++ utils/httpReq.js
@@ -1,3 +1,5 @@
+import { showModal } from "./modal.js";
+
 const BASE_URL = `https://api.openweathermap.org/data/2.5/`;
 const API_KEY = "72b923d635e66c6ea9a19cb74241b300";
 
@@ -19,9 +21,9 @@ const getWeatherData = async (type, data) => {
       } else {
         url = `${BASE_URL}/forecast?lat=${data.latitude}&lon=${data.longitude}&appid=${API_KEY}&units=metric`;
       }
-
+      break;
     default:
-        url = `${BASE_URL}/weather?q=tehran&appid=${API_KEY}&units=metric`;
+      url = `${BASE_URL}/weather?q=tehran&appid=${API_KEY}&units=metric`;
       break;
   }
   try {
@@ -30,10 +32,10 @@ const getWeatherData = async (type, data) => {
     if (+json.cod === 200) {
       return json;
     } else {
-      console.log(json.message);
+      showModal(json.message);
     }
   } catch (error) {
-    console.log("An error occured when fetching data");
+    showModal("An error occured when fetching data");
   }
 };
 
